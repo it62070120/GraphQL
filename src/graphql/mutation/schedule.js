335e20efed174458c7c76d39b1faf3904a94d742
it62070120 <62070120@it.kmitl.ac.th>
2022-05-04 05:33:57 +0700

    edit

--- src/graphql/mutation/schedule.js
+++ src/graphql/mutation/schedule.js
@@ -1,4 +1,4 @@
 import { ScheduleTC } from "../../models/schedule";
 
-
 export const createSchedule = ScheduleTC.getResolver('createOne');
+export const deleteScheduleId = ScheduleTC.getResolver("removeById");
